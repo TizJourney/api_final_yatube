9d86adf412f312dcb8e820ebe55395b0c6370267
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-18 12:29:54 +0300

    feat: перенести старые данные

--- api/urls.py
+++ api/urls.py
@@ -1,5 +1,18 @@
-from django.urls import path
+import rest_framework.authtoken.views as authtoken_views
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from . import views
+
+router = DefaultRouter()
+router.register(
+    r'posts/(?P<post_id>[0-9]+)/comments',
+    views.CommentViewSet, basename='CommentViewSet'
+)
+router.register(r'posts', views.PostsViewSet)
 
 
 urlpatterns = [
+    path('v1/api-token-auth/', authtoken_views.obtain_auth_token),
+    path('v1/', include(router.urls)),
 ]
--- api/views.py
+++ api/views.py
@@ -1,1 +1,33 @@
-# TODO:  Напишите свой вариант
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+
+from posts.models import Post
+from posts.serializers import CommentSerializer, PostSerializer
+
+from .permissions import IsAuthorOrReadOnly
+
+
+class PostsViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommentSerializer
+    permission_classes = [IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        post = get_object_or_404(Post, pk=self.kwargs.get("post_id"))
+        serializer.save(author=self.request.user, post=post)
+
+    def perform_update(self, serializer):
+        serializer.save(author=self.request.user)
