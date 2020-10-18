f985a4a1710beef9c1e74f74f7eff743a5a812d7
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-18 12:30:33 +0300

    feat: убрать старую авторизацию по токену

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,3 @@
-import rest_framework.authtoken.views as authtoken_views
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -13,6 +12,5 @@ router.register(r'posts', views.PostsViewSet)
 
 
 urlpatterns = [
-    path('v1/api-token-auth/', authtoken_views.obtain_auth_token),
     path('v1/', include(router.urls)),
 ]
