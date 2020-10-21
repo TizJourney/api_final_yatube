99867bd524f67fc8c534758e81cc58d41e3c5fec
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-21 16:35:31 +0300

    feat: добавить модели для админки

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Group, Post, Comment, Follow
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'text', 'pub_date', 'author',)
+    search_fields = ('text',)
+    list_filter = ('pub_date',)
+    empty_value_display = '-пусто-'
+
+
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'title', 'slug', 'description',)
+    search_fields = ('title', 'slug',)
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'post', 'text', 'created',)
+    search_fields = ('text',)
+    list_filter = ('created',)
+
+
+class FollowAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'author', 'user',)
+    list_filter = ('author', 'user',)
+
+
+admin.site.register(Group, GroupAdmin)
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment, CommentAdmin)
+admin.site.register(Follow, FollowAdmin)
