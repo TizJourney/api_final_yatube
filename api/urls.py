c4ec7c2cecba7dd624576f351f6e9943870540e0
Konstantin Bondar <kostya.bondar@gmail.com>
2020-10-23 17:26:56 +0300

    feat: вынести v1 urls в отдельную группу v1 паттернов

--- api/urls.py
+++ api/urls.py
@@ -1,7 +1,9 @@
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
-from rest_framework_simplejwt.views import (TokenObtainPairView,
-                                            TokenRefreshView)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from . import views
 
@@ -12,14 +14,14 @@ router.register(
 )
 router.register(r'posts', views.PostsViewSet)
 
+v1_url_patterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('follow/', views.FollowView.as_view()),
+    path('group/', views.GroupView.as_view()),
+]
+
 urlpatterns = [
-    path('v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path(
-        'v1/token/refresh/',
-        TokenRefreshView.as_view(),
-        name='token_refresh'
-    ),
-    path('v1/follow/', views.FollowView.as_view()),
-    path('v1/group/', views.GroupView.as_view()),
+    path('v1/', include(v1_url_patterns)),
     path('v1/', include(router.urls)),
 ]
